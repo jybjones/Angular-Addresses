98415518f222a4b94d40a3a26c065dd1b8b281f0
Jennifer Jones <jybjones@gmail.com>
2015-06-02 10:35:51 -0500

    added a table and data

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,14 @@
 angular
   .module('angularAddresses', [])
 
-  .controller('Main', function ($scope) {
-    $scope.names = [
-      'Ben',
-      'Charity',
-      'Dan',
-      'Elsa',
-      'Amanda'
+  .controller('Main', function () {
+    var vm = this;
+
+    vm.people = [
+      {name: 'Ben', twitter: '@ben123', phone: '+1 615 234 5678'},
+      {name: 'Dan', twitter: '@dandan', phone: '(615) 234-5678'},
+      {name: 'Elsa', twitter: '@letitgo', phone: '615-234-5678'},
+      {name: 'Amanda', twitter: '@princessamanda', phone: '1 615 234 5678'},
+      {name: 'Charity', twitter: '@nonprofit', phone: '615.234.5678'}
     ];
   });
